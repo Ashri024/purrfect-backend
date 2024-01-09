d4c711cd1340d44c8f6cfe271b4721d45df161ba
Ashri024 <ashri.2214504823@mujonline.edu.in>
2024-01-09 20:32:13 +0530

    third

--- src/app.js
+++ src/app.js
@@ -19,4 +19,3 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.set("views", path.join(__dirname, "../templates/views"));
 app.set("view engine", "hbs");
 
-app.listen(port,'0.0.0.0', () => console.log(`Example app listening on port http://localhost:${port} !`))
